ee49c35dcd794747cf145304b72a335cc03e955b
VarunOffical <p03kumars@gmail.com>
2024-08-06 17:20:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let button = document.querySelector('btn')
+
+function clicked(){
+
+    document.getElementById('container').style.transition = '5s'
+    document.getElementById('container').style.display = 'block'
+    console.log("button was clicked");
+}
